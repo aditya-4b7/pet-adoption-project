8bb8ad623e5c852abefee16644db2d8df420bb16
aditya-4b7 <gokulparise@gmail.com>
2026-03-31 21:53:52 -0400

    updating asg

--- modules/asg/outputs.tf
+++ modules/asg/outputs.tf
@@ -1,2 +1,12 @@
 output "asg_name" { value = aws_autoscaling_group.app.name }
 output "app_security_group_id" { value = aws_security_group.app.id }
+
+output "asg_arn" {
+  description = "Auto Scaling Group ARN"
+  value       = aws_autoscaling_group.app.arn
+}
+
+output "launch_template_id" {
+  description = "Launch template ID"
+  value       = aws_launch_template.app.id
+}
