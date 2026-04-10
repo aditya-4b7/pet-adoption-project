3db4513b1baca3ac5e2af9974ce8bd83a1482c2e
aditya-4b7 <gokulparise@gmail.com>
2026-04-10 11:22:56 -0400

    updating sonarqube changes

--- modules/sonarqube/outputs.tf
+++ modules/sonarqube/outputs.tf
@@ -1,2 +1,26 @@
-output "sonarqube_public_ip" { value = aws_instance.sonarqube.public_ip }
-output "sonarqube_url" { value = "http://${aws_instance.sonarqube.public_ip}:9000" }
+# output "sonarqube_public_ip" { value = aws_instance.sonarqube.public_ip }
+# output "sonarqube_url" { value = "http://${aws_instance.sonarqube.public_ip}:9000" }
+output "sonarqube_instance_id" {
+  description = "SonarQube instance ID"
+  value       = aws_instance.sonarqube.id
+}
+
+output "sonarqube_public_ip" {
+  description = "Public IP of SonarQube server"
+  value       = aws_instance.sonarqube.public_ip
+}
+
+output "sonarqube_private_ip" {
+  description = "Private IP of SonarQube server"
+  value       = aws_instance.sonarqube.private_ip
+}
+
+output "sonarqube_url" {
+  description = "SonarQube UI URL"
+  value       = "http://${aws_instance.sonarqube.public_ip}:9000"
+}
+
+output "sonarqube_sg_id" {
+  description = "Security group ID of SonarQube"    
+  value       = aws_security_group.sonarqube.id
+}
