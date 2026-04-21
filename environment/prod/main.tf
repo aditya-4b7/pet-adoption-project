dc1ffbd382bb1e9db3bdb53bd79169970994a2c3
aditya-4b7 <gokulparise@gmail.com>
2026-04-21 03:11:40 -0400

    passing bastion IP

--- environment/prod/main.tf
+++ environment/prod/main.tf
@@ -129,6 +129,10 @@ output "bastion_public_ip" {
   value = module.bastion.bastion_public_ip
 }
 
+output "bastion_private_ip" {
+  value = module.bastion.bastion_private_ip
+}
+
 output "jenkins_url" {
   value = module.jenkins.jenkins_url
 }
